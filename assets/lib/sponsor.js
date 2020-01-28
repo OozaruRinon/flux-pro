8658531dad002267fdff366343ce4ae990b4ea9f
Dean Connolly <31525346+dconnolly95@users.noreply.github.com>
2020-01-28 16:57:33 +1000

    Last enhancements
    Final upload of enhancements and fixes

--- assets/lib/sponsor.js
+++ assets/lib/sponsor.js
@@ -64,6 +64,6 @@ $("#sample").click(function () {
 	var hyperlinkText= $("#hyperlinkText").val();
 	var hyperlink= $("#hyperlink").val();
 
-	$( "#sampleAd" ).html('<img src="' + imgLink + '" height="30" width="30"> ' + text + ' <a target="_blank" href="' + hyperlink + '" rel="nofollow" title="Links to an External Advertiser site" target="_blank"> <b>' + hyperlinkText + '</b></a><div id="beacon_2a55ce0186" style="position: absolute; left: 0px; top: 0px; visibility: hidden;"><img src="https://gen.etherscan.io/www/d/lg.php?ebannerid=5&amp;campaignid=5&amp;zoneid=2&amp;loc=https%3A%2F%2Fetherscan.io%2F&amp;cb=2a55ce0186" width="0" height="0" alt="" style="width: 0px; height: 0px;"></div>');
+	$( "#sampleAd" ).html('<img src="' + imgLink + '" height="30" width="30" class="img-fluid rounded-square shadow-lg"> ' + text + ' <a target="_blank" href="' + hyperlink + '" rel="nofollow" title="Links to an External Advertiser site" target="_blank"> <b>' + hyperlinkText + '</b></a><div id="beacon_2a55ce0186" style="position: absolute; left: 0px; top: 0px; visibility: hidden;"><img src="https://gen.etherscan.io/www/d/lg.php?ebannerid=5&amp;campaignid=5&amp;zoneid=2&amp;loc=https%3A%2F%2Fetherscan.io%2F&amp;cb=2a55ce0186" width="0" height="0" alt="" style="width: 0px; height: 0px;"></div>');
 
 })
